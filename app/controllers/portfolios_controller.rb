01fe4014eb6a4a8d552f8bee162373355c7bebf4
erockk <imac@imacs-iMac.local>
2018-12-09 09:46:01 -0500

    Added new create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,8 +3,29 @@ class PortfoliosController < ApplicationController
 
 	def index
 		@portfolio_items = Portfolio.all 
-
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+
+		respond_to do |format|
+			if @portfolio_item.save
+			    format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+			    
+			else
+			    format.html { render :new }
+
+			end
+		end
+	end
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end		
 
 
 
