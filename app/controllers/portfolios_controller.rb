adb0d2f7520fce8eacfe3ad5cb742ed9aad3a2b6
erockk <imac@imacs-iMac.local>
2018-12-09 11:18:15 -0500

    Added new and edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,32 +1,42 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-	def index
-		@portfolio_items = Portfolio.all 
-	end
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-	def new
-		@portfolio_item = Portfolio.new
-	end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
-	def create
-		@portfolio_item = Portfolio.new(portfolio_params)
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-		respond_to do |format|
-			if @portfolio_item.save
-			    format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-			    
-			else
-			    format.html { render :new }
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
-			end
-		end
-	end
-
-	def portfolio_params
-		params.require(:portfolio).permit(:title, :subtitle, :body)
-	end		
-
-
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 
 end
