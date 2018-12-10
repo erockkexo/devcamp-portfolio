81bc85343d4d2ccaf4b2bc57285e754ae3e193ff
erockk <imac@imacs-iMac.local>
2018-12-10 12:58:04 -0500

    Implemented validations for blogs, portfolios and skills

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, publish: 1 }
 	extend FriendlyId
 	friendly_id :title, use: :slugged
+
+	#wont create without a title and body
+	validates_presence_of :title, :body
 end
