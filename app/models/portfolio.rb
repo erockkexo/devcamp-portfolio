5b42f896c79805e1fba998990fb46adbe63ce9c1
erockk <imac@imacs-iMac.local>
2018-12-11 07:37:50 -0500

    Implemented default settings for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,15 @@ class Portfolio < ApplicationRecord
 	def self.ruby
 		where( subtitle: "Ruby on Rails")
 	end
+
+	#after a instance has been initialized we can set the defaults
+	after_initialize :set_defaults
+
+	#create the method "set_defaults"
+	def set_defaults
+		#when ever you are creating a new portfolio item (nothing else)
+		#||= means if nil, replace with this:
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
