6a596649a81a7a211c11f45bf2bcce598c246230
erockk <imac@imacs-iMac.local>
2018-12-11 07:58:57 -0500

    implemented concern for managin image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
 
+include Placeholder
+
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	def self.angular
@@ -17,7 +19,7 @@ class Portfolio < ApplicationRecord
 	def set_defaults
 		#when ever you are creating a new portfolio item (nothing else)
 		#||= means if nil, replace with this:
-		self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "http://placehold.it/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,16 @@
 class Skill < ApplicationRecord
 
+#to bring in the placeholder concern => placeholder.rb
+include Placeholder
+
 	validates_presence_of :title, :percent_utilized
+
+	#We started with adding a database entry: rails g migrations add_badge_to_skills
+
+	#now we want to add defaults 
+	after_initialize :set_defaults 
+
+	def set_defaults 
+		self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+	end
 end
