6fd6282be83f74e2033cbcb4c33221413cf8054a
erockk <imac@imacs-iMac.local>
2018-12-12 16:53:06 -0500

    added sessions tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'angular_items', to: 'portfolios#angular'
 
+  # This a specific route that takes in an ID paramater and is mapped to the show action
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
