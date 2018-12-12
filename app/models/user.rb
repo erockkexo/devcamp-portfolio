336d0fd9d3dca05dc0a7064bcf82fd25095d82d6
erockk <imac@imacs-iMac.local>
2018-12-12 12:44:07 -0500

    Completed authentication with virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # we are going to pull the first name and last name from the entry point of the user
+  # we also should include a validation of a name so the customer can not skip it by accident 
+
+  validates_presence_of :name 
+
+  def first_name 
+  	self.name.split.first 
+  end
+
+  def last_name
+  	self.name.split.last
+  end 
 end
