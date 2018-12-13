4cc9d6f47e4f26f342d51a26c6fc9089a1fe365a
erockk <imac@imacs-iMac.local>
2018-12-13 09:09:23 -0500

    added nav bar items and added default header content

---
+++ app/controllers/concerns/blogs_pagetitle.rb
@@ -0,0 +1,17 @@
+module BlogsPagetitle
+
+	extend ActiveSupport::Concern
+
+
+	included do 
+		before_action :set_title
+	end
+
+	def set_title
+
+		@page_title =  "Devcamp Portfolio | My Portfolio Website"
+		@seo_keywords = "Eric's Portfolio"
+		
+	end		
+
+end
