26b5028777bf23e626a534cc64ba5407337b267d
erockk <imac@imacs-iMac.local>
2018-12-13 13:09:17 -0500

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def sample_helper
+		"<p>My helper</p>".html_safe
+	end
+
+	def user_check
+
+    if current_user.is_a?(User) 
+    	link_to "Logout", destroy_user_session_path, method: :delete 
+    else 
+    	( link_to "Log in", new_user_session_path ) + "<br>".html_safe +
+    	( link_to "Sign up", new_user_registration_path )
+     end 
+	end
+
 end
