fada5df516309753c3af6f614e6439991d58b497
erockk <imac@imacs-iMac.local>
2018-12-15 09:42:40 -0500

    Created the copyright tool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < ActionController::Base
 	include CurrentUserConcern
 	include BlogsPagetitle
 
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = DevcampViewTool::Renderer.copyright "Eric Acri", "All rights reserved"
+	end
 
 
+end
+
+
+module DevcampViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
+		end
+	end
 end
